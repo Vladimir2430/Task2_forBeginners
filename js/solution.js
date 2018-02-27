cdfb5db58321d715286288cf90be401d050f59c2
Uladzimir <berkut2430@gmail.com>
2018-02-27 23:51:14 +0300

    start

--- js/solution.js
+++ js/solution.js
@@ -11,7 +11,14 @@
      */
     function solution(map) {
         // todo: подсчитать кол-во островов на карте
-        return 0;
+        let count=0;
+        for (let i=0; i<map.length; i++) {
+          for (let j=0; j<map[0].length; j++) {
+            if (map[i][j]==1 && (map[i][j+1]==1 || map[i][j-1]==1)) {continue;}
+            if (map[i][j]==1 && (map[i][j+1]==0 || map[i][j-1]==0)) {count+=1;}
+          }
+        }
+        return count;
     }
 
     root.SHRI_ISLANDS.solution = solution;
