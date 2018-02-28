03aa03d826478b1628dfb573e1eba1ec18b2d5e6
Uladzimir <berkut2430@gmail.com>
2018-02-28 12:37:41 +0300

    solution fix short but big_O x^2

--- js/solution.js
+++ js/solution.js
@@ -13,27 +13,19 @@
         // todo: подсчитать кол-во островов на карте
         let count=0;
         let arr=map;
-        for (let i=0; i<map.length; i++) {
-          if ((arr[0][i]==1 || arr[0][i]==2) && arr[0][i+1]==1) {arr[0][i+1]=2;}
-          if ((arr[0][i]==1 || arr[0][i]==2) && arr[1][i]==1) {arr[1][i]=2;}
-          if ((arr[0][i]==1) && (arr[0][i+1]==0 || arr[0][i+1]==2 || arr[0][i+1]==undefined)) {count+=1;}
-
-          if ((arr[1][i]==1 || arr[1][i]==2) && arr[1][i+1]==1) {arr[1][i+1]=2;}
-          if ((arr[1][i]==1 || arr[1][i]==2) && arr[2][i]==1) {arr[2][i]=2;}
-          if ((arr[1][i]==1) && (arr[1][i+1]==0 || arr[1][i+1]==2 || arr[1][i+1]==undefined)) {count+=1;}
-
-          if ((arr[2][i]==1 || arr[2][i]==2) && arr[2][i+1]==1) {arr[2][i+1]=2;}
-          if ((arr[2][i]==1 || arr[2][i]==2) && arr[3][i]==1) {arr[3][i]=2;}
-          if ((arr[2][i]==1) && (arr[2][i+1]==0 || arr[2][i+1]==2 || arr[2][i+1]==undefined)) {count+=1;}
-
-          if ((arr[3][i]==1 || arr[3][i]==2) && arr[3][i+1]==1) {arr[3][i+1]=2;}
-          if ((arr[3][i]==1 || arr[3][i]==2) && arr[4][i]==1) {arr[4][i]=2;}
-          if ((arr[3][i]==1) && (arr[3][i+1]==0 || arr[3][i+1]==2 || arr[3][i+1]==undefined)) {count+=1;}
-
-          if ((arr[4][i]==1 || arr[4][i]==2) && arr[4][i+1]==1) {arr[4][i+1]=2;}
-          if ((arr[4][i]==1) && (arr[4][i+1]==0 || arr[4][i+1]==2 || arr[4][i+1]==undefined)) {count+=1;}
+        for (let i=0; i<map.length-1; i++) {
+          for (let j=0; j<map[0].length; j++) {
+            if ((arr[i][j]==1 || arr[i][j]==2) && arr[i][j+1]==1) {arr[i][j+1]=2;} //за 1 или 2 - 2
+            if (arr[i][j]==1 && (arr[i][j+1]==0 || arr[i][j+1]==undefined || arr[i][j+1]==2)) {count+=1;}
+            if ((arr[i][j]==1 || arr[i][j]==2) && arr[i+1][j]==1) {arr[i+1][j]=2;} //под 2 - 2
+          }
         }
-        return count;
+        let sum=0;
+        for (let i=0; i<map[0].length; i++) {
+          if ((map[map.length-1][i]==1 || map[map.length-1][i]==2) && map[map.length-1][i+1]==1) {map[map.length-1][i+1]==2}
+          if (map[map.length-1][i]==1) {sum+=1;}
+        }
+        return count+sum;
     }
 
     root.SHRI_ISLANDS.solution = solution;
