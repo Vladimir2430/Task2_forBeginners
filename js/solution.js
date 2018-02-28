c48b7c9585d97cc75fa981138d58cedc35a1845c
Uladzimir <berkut2430@gmail.com>
2018-02-28 12:43:56 +0300

    solution wish big_O==x^2 update

--- js/solution.js
+++ js/solution.js
@@ -12,12 +12,11 @@
     function solution(map) {
         // todo: подсчитать кол-во островов на карте
         let count=0;
-        let arr=map;
         for (let i=0; i<map.length-1; i++) {
           for (let j=0; j<map[0].length; j++) {
-            if ((arr[i][j]==1 || arr[i][j]==2) && arr[i][j+1]==1) {arr[i][j+1]=2;} //за 1 или 2 - 2
-            if (arr[i][j]==1 && (arr[i][j+1]==0 || arr[i][j+1]==undefined || arr[i][j+1]==2)) {count+=1;}
-            if ((arr[i][j]==1 || arr[i][j]==2) && arr[i+1][j]==1) {arr[i+1][j]=2;} //под 2 - 2
+            if ((map[i][j]==1 || map[i][j]==2) && map[i][j+1]==1) {map[i][j+1]=2;} //за 1 или 2 - 2
+            if (map[i][j]==1 && (map[i][j+1]==0 || map[i][j+1]==undefined || map[i][j+1]==2)) {count+=1;}
+            if ((map[i][j]==1 || map[i][j]==2) && map[i+1][j]==1) {map[i+1][j]=2;} //под 1 или 2 - 2
           }
         }
         let sum=0;
