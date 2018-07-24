c80a3ea7fb1d7ab0aa18f5edf3046c22f4d09d98
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-07-24 18:52:43 +0300

    code improved

--- js/solution.js
+++ js/solution.js
@@ -11,20 +11,20 @@
      */
     function solution(map) {
         // todo: подсчитать кол-во островов на карте
-        let count=0;
-        for (let i=0; i<map.length-1; i++) {
-          for (let j=0; j<map[0].length; j++) {
-            if ((map[i][j]==1 || map[i][j]==2) && map[i][j+1]==1) {map[i][j+1]=2;} //за 1 или 2 - 2
-            if (map[i][j]==1 && (map[i][j+1]==0 || map[i][j+1]==undefined || map[i][j+1]==2)) {count+=1;}
-            if ((map[i][j]==1 || map[i][j]==2) && map[i+1][j]==1) {map[i+1][j]=2;} //под 1 или 2 - 2
+        let count = 0;
+        for (let i = 0; i < map.length-1; i++) {
+          for (let j = 0; j < map[0].length; j++) {
+            if ((map[i][j] === 1 || map[i][j] === 2) && map[i][j + 1] === 1) { map[i][j + 1] = 2; } //за 1 или 2 - 2
+            if (map[i][j] === 1 && (map[i][j + 1] === 0 || map[i][j + 1] === undefined || map[i][j + 1] === 2)) { count += 1; }
+            if ((map[i][j] === 1 || map[i][j] === 2) && map[i + 1][j] === 1) { map[i + 1][j] = 2; } //под 1 или 2 - 2
           }
         }
-        let sum=0;
-        for (let i=0; i<map[0].length; i++) {
-          if ((map[map.length-1][i]==1 || map[map.length-1][i]==2) && map[map.length-1][i+1]==1) {map[map.length-1][i+1]==2}
-          if (map[map.length-1][i]==1) {sum+=1;}
+        let sum = 0;
+        for (let i = 0; i < map[0].length; i++) {
+          if ((map[map.length - 1][i] === 1 || map[map.length - 1][i] === 2) && map[map.length - 1][i + 1] === 1) { map[map.length - 1][i + 1] === 2; }
+          if (map[map.length - 1][i] === 1) { sum += 1; }
         }
-        return count+sum;
+        return count + sum;
     }
 
     root.SHRI_ISLANDS.solution = solution;
